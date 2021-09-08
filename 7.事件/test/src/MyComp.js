9eba773b778c62765ece090ff092eb065ebbac02
zmplly <1776321381@qq.com>
2021-09-08 23:09:38 +0800

    事件

---
+++ 7.事件/test/src/MyComp.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MyComp extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
--- 7.事件/test/src/index.js
+++ 7.事件/test/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const btn = <button >点击</button>
+function handleClick() {
+  console.log('点击了。。。。')
+}
+
+
+const btn = <button onClick={handleClick} onMouseEnter={()=>{console.log('鼠标移入了')}}>点击</button>
 
 ReactDOM.render(
   btn,
