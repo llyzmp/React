e194183c5ef16ffc16a9dcfcc48609e282dc1884
zmplly <1776321381@qq.com>
2021-10-20 23:21:10 +0800

    react-router

--- 999.react-router/react-router/src/App.js
+++ 999.react-router/react-router/src/App.js
@@ -1,13 +1,27 @@
 import React from 'react'
 
-// import FormTest from './components/FormTest'
-import ReForward from './components/ReForward'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+function A() {
+  return <h1>组件A</h1>
+}
+
+function B() {
+  return <h1>组件B</h1>
+}
+
+function C() {
+  return <h1>组件C</h1>
+}
 
 export default function App() {
   return (
-    <div>
-      <ReForward> </ReForward>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/a" component={A} />
+        <Route path="/a/b" exact component={B} />
+        <Route path="/a/c" exact component={C} />
+      </Switch>
+    </Router>
   )
 }
