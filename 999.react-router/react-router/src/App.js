8fb815f78b68f986324c700bf81e23c884938c2c
zmplly <1776321381@qq.com>
2021-10-20 22:12:33 +0800

    router

---
+++ 999.react-router/react-router/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// import FormTest from './components/FormTest'
+import ReForward from './components/ReForward'
+
+
+export default function App() {
+  return (
+    <div>
+      <ReForward> </ReForward>
+    </div>
+  )
+}
