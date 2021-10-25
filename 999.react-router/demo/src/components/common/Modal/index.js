38f61cc32b8483d4abe279fbea4b56d42aa79cbe
zmplly <1776321381@qq.com>
2021-10-25 23:37:34 +0800

    后台管理模板

---
+++ 999.react-router/demo/src/components/common/Modal/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './index.css'
+export default function Modal(props) {
+  var defaultProps = {
+    bg: "rgba(0,0,0,.5)"
+  }
+  var datas = Object.assign({},defaultProps,props)
+  return (
+    <div onClick={e => {
+      if(e.target.className === 'modal') {
+        datas.onClose();
+      }
+    }}  className="modal" style={{ background: datas.bg}} >
+      <div className="modal-center">
+        {datas.children}
+      </div>
+    </div>
+  )
+}
